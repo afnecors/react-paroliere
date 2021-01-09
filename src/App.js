2bb6ba6239b9fe8ed74e3e3e98f8eae48857cdc5
Francesco Serra <afnecors@gmail.com>
2021-01-09 11:48:08 +0100

    Create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
+import dice_list from './data';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Face(props) {
+    return (
+        <div className="dice">
+            <div className="inner">
+                <div className="letter">{props.dice[Math.floor(6 * Math.random())]}</div>
+            </div>
+        </div>
+    );
+}
+
+class App extends React.Component {
+
+    shuffle(dice) {
+        var j, x, i;
+        for (i = dice.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = dice[i];
+            dice[i] = dice[j];
+            dice[j] = x;
+        }
+        return dice;
+    }
+
+    newGame() {
+        this.forceUpdate();
+    }
+
+    render() {
+        return (
+            <div id="container">
+                <div className="wrapper" onClick={this.newGame.bind(this)}>
+                    {this.shuffle(dice_list).map(dice => (
+                        <Face dice={dice} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
